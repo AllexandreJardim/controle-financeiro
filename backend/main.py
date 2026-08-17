7b177b81f3b7d351a4367aa9fe1e898dd3216ba0
Alexandre <alexandrejsouza83@gmail.com>
2026-08-17 15:05:13 -0300

    feat:adiciona cadastro de movimentacoes

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,65 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from database import get_connection
 
 
 app = FastAPI()
 
 
+class Movimentacao(BaseModel):
+    tipo: str
+    descricao: str
+    valor: float
+    categoria: str
+    data: str
+
+
 @app.get("/")
 def root():
     return {"mensagem": "API do Controle Financeiro funcionando!"}
+
+
+@app.get("/movimentacoes")
+def listar_movimentacoes():
+    conexao = get_connection()
+    cursor = conexao.cursor()
+
+    cursor.execute("SELECT * FROM movimentacoes")
+    movimentacoes = cursor.fetchall()
+
+    conexao.close()
+
+    return [dict(movimentacao) for movimentacao in movimentacoes]
+
+
+@app.post("/movimentacoes")
+def criar_movimentacao(movimentacao: Movimentacao):
+    conexao = get_connection()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        INSERT INTO movimentacoes
+        (tipo, descricao, valor, categoria, data)
+        VALUES (?, ?, ?, ?, ?)
+    """, (
+        movimentacao.tipo,
+        movimentacao.descricao,
+        movimentacao.valor,
+        movimentacao.categoria,
+        movimentacao.data
+    ))
+
+    conexao.commit()
+
+    novo_id = cursor.lastrowid
+
+    conexao.close()
+
+    return {
+        "id": novo_id,
+        "tipo": movimentacao.tipo,
+        "descricao": movimentacao.descricao,
+        "valor": movimentacao.valor,
+        "categoria": movimentacao.categoria,
+        "data": movimentacao.data
+    }
