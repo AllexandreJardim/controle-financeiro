4791c28f2ff7aee62b10aa4afc87779fc0e75207
Alexandre <alexandrejsouza83@gmail.com>
2026-08-17 15:51:43 -0300

    fix:corrige script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 const API_URL = 'http://127.0.0.1:8000';
 
 let movimentacoes = [];
